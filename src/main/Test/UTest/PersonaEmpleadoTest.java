2869f8a2f4e9b5d4b342188f1a9ba2193995bdab
ezequieldjc <c3l9s3b7>
2020-07-19 17:16:07 -0300

    add new test

---
+++ src/main/Test/UTest/PersonaEmpleadoTest.java
@@ -0,0 +1,14 @@
+package UTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaEmpleadoTest {
+
+    @Test
+    public void test(){
+        assertEquals(18,18);
+    }
+
+}
--- src/main/Test/UTest/TestExample.java
+++ src/main/Test/UTest/TestExample.java
@@ -3,11 +3,13 @@ package UTest;
 import Data.DataConnectioniMac;
 import Entities.Persona.PersonaEmpleado;
 import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
 public class TestExample extends TestCase {
 
     private PersonaEmpleado p;
 
+    @Test
     public void test(){
         assertEquals(4,4);
     }
